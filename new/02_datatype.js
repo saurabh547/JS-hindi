1233747fb26fb03340fe2e95b223f74367a33be5
Saurabh Gaud <44631647+saurabh547@users.noreply.github.com>
2024-03-19 13:07:16 +0000

    DataTypes

---
+++ new/02_datatype.js
@@ -0,0 +1,34 @@
+"use strict";
+
+// const name = "saurabh";
+// console.log(name);
+
+/*
+Number
+string
+boolean
+bigint
+symbol
+undefiend
+null 
+object
+*/ 
+
+// In Javascript there is two type of data types Primitive and non-primitive
+
+/*Primitive Data Type
+String
+Number
+Boolean
+symbol
+null
+undefiend
+*/
+
+/* Non Primitive data type 
+Object
+Array 
+*/
+
+const age = 25;
+console.log(typeof age);
