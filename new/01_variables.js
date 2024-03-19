c668f8f86615f58087e714ccc45ec38621f600c4
Saurabh Gaud <44631647+saurabh547@users.noreply.github.com>
2024-03-19 12:29:09 +0000

    variables

---
+++ new/01_variables.js
@@ -0,0 +1,8 @@
+const employeeId = 12; // constant variable it not be change
+let employeeName = "saurabh";
+var employeeSalary = "12k";
+phoneNumber = "9601863017";
+
+// dont't use var because it is create problem in block scope and functional scope
+// phoneNumber = 'dd'; changeble
+console.table([employeeId,employeeName,employeeSalary,phoneNumber])
