872f872ce1e1b900eb138a1e1af6cd1e5ad87a40
Saurabh Gaud <44631647+saurabh547@users.noreply.github.com>
2024-03-20 09:49:55 +0000

    Conversion Datatype

---
+++ new/03_conversionOperation.js
@@ -0,0 +1,23 @@
+// const score = "true";
+// const convertScore = Number(score);
+// console.log(convertScore);
+// console.log(typeof convertScore);
+
+// "33" => 33
+// "33abc" => NaN
+// true =>1; false => 0
+
+// let isLoggedIn = "sa";
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "saurabh" => true
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
