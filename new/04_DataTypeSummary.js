4374699de900d716afee4de5c0ab3b415fd28b65
Saurabh Gaud <44631647+saurabh547@users.noreply.github.com>
2024-03-21 07:10:40 +0000

    datatype

---
+++ new/04_DataTypeSummary.js
@@ -0,0 +1,16 @@
+// primitive Data Type
+// Number
+// Boolean
+// String
+// Null
+// Undefined
+// Symbol
+// BigInt
+
+// Non-Primitive(Refrence Data Type)
+
+// Array
+// Objects
+// Functions
+
+// JavaScript is Dynamically type language
