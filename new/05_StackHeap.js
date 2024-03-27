38698d49540bfb95de8c9a5e2632df9cff74907f
Saurabh Gaud <44631647+saurabh547@users.noreply.github.com>
2024-03-27 06:34:04 +0000

    Memory study

---
+++ new/05_StackHeap.js
@@ -0,0 +1,28 @@
+// ************************Memory************************
+
+// Stack (primitive Data Type)
+// Heap (Non primitive Data type)
+
+
+// Stack not give refrence of meamory and not changing in original value it gives a copy of original one
+// let name = "saurabh"
+// let anotherName = name
+// anotherName = "shubham"
+
+// console.log(anotherName);
+// console.log(name);
+
+
+// Heap (non primitive Data type)
+
+// let name = {
+//     email:"saurabh547@gmal.com",
+//     rollNo:21
+// }
+
+// let newName = name;
+// newName.email = "gaudsaurabh@gmail.com"
+// console.log(newName.email)
+// console.log(name.email);
+
+// Heap memory change in original 
