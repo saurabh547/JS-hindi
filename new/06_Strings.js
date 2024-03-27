72fd9b1f685896e2c3b99438e77bb14b2e80167b
Saurabh Gaud <44631647+saurabh547@users.noreply.github.com>
2024-03-27 15:01:10 +0000

    string methods

---
+++ new/06_Strings.js
@@ -0,0 +1,43 @@
+// let name = "saurabh"
+// let lname = "gaud"
+
+// let fname = `my name is ${name} and my surname is ${lname}`
+// // console.log(fname);
+
+// const laxmi = new String("mata");
+// console.log(typeof laxmi);
+
+// const newLaxmi = laxmi.substring(0,4)
+// console.log(newLaxmi);
+
+// console.log(laxmi.charAt(2));
+
+// const anotherString = laxmi.slice(-4,1)
+// console.log(anotherString);
+
+// trim method
+// const newName = "         saurabh      ";
+// const afterTrim = newName.trim();
+// console.log(afterTrim);
+
+// const url = `https://congenial%20dollop%20qpv59gqqgjxc95p4.github.dev/`
+// const replace = url.replace('%20','-');
+// console.log(replace);
+
+// console.log(url.includes('github')); // it checks keyword in that variable 
+
+
+// Split method in String
+
+// const fullName = "my name is saurabh gaud kanhaiyalal and i'm leaving in umbergaon";
+
+// const afterSplit = fullName.split(' ');
+// console.log( afterSplit[1]);
+
+// const name = "saurabh-bhai-hai";
+// console.log(name.split('-'));
+
+// const newName = name.split('-');
+// console.log( newName[1])
+
+
