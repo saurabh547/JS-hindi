65fdd90e156e2aa9729647817454455ed62b6d8d
Saurabh Gaud <44631647+saurabh547@users.noreply.github.com>
2024-03-28 08:21:56 +0000

    Number Methds()

---
+++ new/07_Number.js
@@ -0,0 +1,41 @@
+const score = 100;
+// console.log(typeof score);
+
+const balance = new Number(500);
+// console.log(balance)
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2)); // toFixed is add a 00 after main value
+
+const otherValue = 120.4552;
+
+// console.log(otherValue.toPrecision(5));
+
+const localString = 1000000;
+// console.log(localString.toLocaleString('en-IN'));
+
+
+// ******************** MATHS ***************************************
+
+// console.log(Math);
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.4));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.2));
+
+// console.log(Math.sqrt(5));
+
+// console.log(Math.min(4,5,2))
+// console.log(Math.max(4,5,2))
+
+// console.log((Math.random()*10) + 1);
+// console.log(Math.floor(Math.random()*10)+1)
+
+// const min = 10
+// const max = 20
+
+// console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+// const arr = [1,8,2,7,8];
+// console.log(arr.sort())
